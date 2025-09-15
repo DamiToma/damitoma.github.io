b22aaeb797c336edaefcfe5852f70fd3419e9fba
Damiano Tomazzolli <damiano.tomazzolli@gmail.com>
2025-09-15 08:00:22 +0200

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,23 @@ import { ThemeProvider } from "next-themes";
 
 import type { AppProps } from 'next/app';
 
+
+import Head from 'next/head';
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
