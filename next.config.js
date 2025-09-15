8da56f4c6d0bfb99ab99f3023bb0c548c0ef7180
Damiano Tomazzolli <damiano.tomazzolli@gmail.com>
2025-09-15 07:54:02 +0200

    Add .nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',          // static export for GitHub Pages
+  images: { unoptimized: true }, // no server image optimization on Pages
+  trailingSlash: true,       // makes static hosting friendlier (…/page/index.html)
 };
-
 module.exports = nextConfig;
